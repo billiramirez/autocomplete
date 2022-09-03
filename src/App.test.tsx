34844ebf3b61286fa221915c57f3adde71d51bd2
Billi Ramirez <ramirezbilly42@outlook.com>
2022-09-03 08:57:58 -0600

    refs (chore) : Remove unused code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import AutoComplete from "./library/AutoComplete";
 
 test("render App", () => {
   render(<App />);
